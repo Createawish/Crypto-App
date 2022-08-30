0e4508227739463bbbde0e1aafb00d89f912df3f
Createawish <bezlyudovalera1996@gmail.com>
2022-08-31 00:29:02 +0300

    Feature: add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import {Layout, Typography,Space} from 'antd';
-import {NavBar }from "./component";
+import {NavBar}from "./component";
+import { Exchanges, Home, Cryptocurrencies, News, CryptoDetails} from './pages/index'
 import './App.css';
 const App = () => {
     return (
@@ -10,7 +11,27 @@ const App = () => {
                 <NavBar/>
             </div>
             <div classNmae='main'>
-
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route exact path='/'>
+                                <Home/>
+                            </Route>
+                            <Route exact path='/exchange'>
+                                <Exchanges/>
+                            </Route>
+                            <Route exact path='/cryptocurrencies'>
+                                <Cryptocurrencies/>
+                            </Route>
+                            <Route exact path='/crypto/:coinId'>
+                                <CryptoDetails/>
+                            </Route>
+                            <Route exact path='/news'>
+                                <News/>
+                            </Route>
+                        </Routes>
+                    </div>
+                </Layout>
             </div>
             <div className='footer'>
 
